926a36870bd0103683d3547d1f8c2ab46beafbb5
Madixxx22 <vysokolyan2016@gmail.com>
2022-11-22 20:37:21 +0300

    Add option argument

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
-gendiff:
+gendiff-help:
 	poetry run gendiff -h
+
+gendiff:
+	poetry run gendiff
+
+build:
+	poetry build
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,8 +3,9 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
-    parser.add_argument("first_file", type=str, default="", )
+    parser.add_argument("first_file", type=str, default="")
     parser.add_argument("second_file", type=str, default="")
+    parser.add_argument("-f", "--format", type=str, default="json", help="set format of output")
     args = parser.parse_args()
 
 if __name__ == '__main__':
